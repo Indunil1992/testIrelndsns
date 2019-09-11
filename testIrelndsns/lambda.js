9bd2c842f62f83c9d6476f57a13ffe3befa10470
Indunil1992 <indunil@adroitlogic.com>
2019-09-11 17:43:47 +0530

    hioo

--- testIrelndsns/lambda.js
+++ testIrelndsns/lambda.js
@@ -1,18 +1,32 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
-    sns.listSubscriptionsByTopic({
-        TopicArn: 'arn:aws:sns:eu-west-1:318300609668:testinddR'
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/testsqs`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
     }).promise()
-        .then(data => {
-        console.log("dataaaa");
-        console.log(data);
-            // your code goes here
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    console.log("receivedMsgData");
+                    console.log(receivedMsgData);
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                console.log("noo msges");
+                // No messages to process
+            }
         })
         .catch(err => {
-            console.log("errrrr");
-            console.log(err);
+            console.log("errrrrrrrr");
+           console.log(err);
             // error handling goes here
         });
 
